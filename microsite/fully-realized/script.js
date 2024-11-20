6bccd3738821b0b145b37c18af32beb55178c6e6
Angela Liu <64173438+alegnauil@users.noreply.github.com>
2024-11-19 21:09:29 -0600

    half fully realized

---
+++ microsite/fully-realized/script.js
@@ -0,0 +1,39 @@
+// Nav bar
+    //load
+$(function(){
+    $("#nav-placeholder").load("nav.html");
+    });
+
+$(document).ready(function() {
+    // open
+        $('.travel_btn').click(function() {
+            $('#nav-placeholder').slideToggle();
+        });
+
+    //close
+        $(document).on('click', '.close_btn', function() {
+            $('#nav-placeholder').slideUp();
+        });
+    });
+
+    //highlight NOT WORKING
+    $(function() {
+        // Get the current file name from the URL
+        let currentPath = window.location.pathname.split('/').pop(); // Get the last part of the URL
+        console.log(currentPath)
+        // Loop through all nav links and check if they match the current path
+        $('nav .option a').each(function() {
+            const href = $(this).attr('href'); // Get href of each link
+            console.log(href);
+            if (href === currentPath) {
+                $(this).addClass('active'); // Add 'active' class to the matching link
+                console.log('Added active to:', href); // Debug
+            } else {
+                $(this).removeClass('active'); // Remove 'active' class from non-matching links
+                console.log('not added active to:', href);
+            }
+        });
+    });
+// End of Nav Bar
+
+
